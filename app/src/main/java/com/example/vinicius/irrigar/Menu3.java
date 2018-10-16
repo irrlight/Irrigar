38f3196e3a4d0f1d8d6cacaf1d8215bd4e44b8d6
Vinicius <mvsl072@gmail.com>
2018-10-15 23:26:20 -0300

    Irrigação

--- app/src/main/java/com/example/vinicius/irrigar/Menu3.java
+++ app/src/main/java/com/example/vinicius/irrigar/Menu3.java
@@ -1,21 +1,24 @@
 package com.example.vinicius.irrigar;
 
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.support.v4.app.Fragment;
-import java.util.Calendar;
 import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.Toast;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class Menu3 extends Fragment{
@@ -38,5 +41,7 @@ public class Menu3 extends Fragment{
     }
 
 
+    }
 
-}
+
+
